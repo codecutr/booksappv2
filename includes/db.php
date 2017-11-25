137fca658ed2b756844e1300adfd27596674af35
Meg Fryling <mfryling@siena.edu>
2017-11-25 11:58:38 -0500

    Add files via upload

---
+++ includes/db.php
@@ -0,0 +1,21 @@
+<?php 
+    // A simple PHP script demonstrating how to connect to MySQL.
+    // Press the 'Run' button on the top to start the web server,
+    // then click the URL that is emitted to the Output tab of        
+    //the console.
+
+    $servername = getenv('IP');
+    $username = getenv('C9_USER');
+    $password = "";
+    $database = "c9";
+    $dbport = 3306;
+
+    // Create connection
+    $mysqli = new mysqli($servername, $username, $password, $database, $dbport);
+
+    // Check connection
+    if ($mysqli->connect_error) {
+        die("Connection failed: " . $mysqli->connect_error);
+    } 
+    //echo "Connected successfully (".$mysqli->host_info.")";
+?>
